c21b45e2ceddfdd2c7426120733ba7722722b1aa
Spatarantula <98459032+Spatarantula@users.noreply.github.com>
2024-04-02 12:02:47 +0800

    restart server logic

--- simple_fuzzing.py
+++ simple_fuzzing.py
@@ -6,6 +6,18 @@ import socket
 import random
 import string
 from time import sleep
+from time import time
+import subprocess
+
+timeout = time() + 60
+
+def restart_server():
+    command = ["python", "coapserver.py"] 
+    try:
+        subprocess.Popen(command)
+        print("CoAP server restarted")
+    except Exception as e:
+        print("Error restarting CoAP server: ", str(e))
 
 class CoAPFuzzer:
     def __init__(self, host, port):
@@ -13,7 +25,7 @@ class CoAPFuzzer:
         self.port = port
         self.client = HelperClient(server=(self.host, self.port))
         self.original_payload = "Hello, CoAP!"
-
+            
     def fuzz_payload(self, payload, num_bytes):
         # Generate random bytes to replace part of the payload
         fuzz_bytes = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(num_bytes))
@@ -21,33 +33,42 @@ class CoAPFuzzer:
 
     def fuzz_and_send_requests(self, num_requests, num_bytes):
         while True:
-            fuzzed_payload = self.fuzz_payload(self.original_payload, num_bytes)
-            print("Fuzzing payload:", fuzzed_payload)
+            if time() > timeout:
+                break
+            try:
+                fuzzed_payload = self.fuzz_payload(self.original_payload, num_bytes)
+                print("Fuzzing payload:", fuzzed_payload)
 
-            # Send fuzzed GET request with the fuzzed payload and path "/basic/"
-            req = Request()
-            serializer = Serializer()
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            req.type = random.choice([defines.Types["CON"], defines.Types["NON"], defines.Types["ACK"], defines.Types["RST"]])
-            req.code = random.choice([defines.Codes.EMPTY.number, defines.Codes.GET.number, defines.Codes.POST.number, defines.Codes.PUT.number, defines.Codes.DELETE.number]) # Everytime EMPTY is chosen, the server will give up, but not crash
-            req.token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(1, 100))) # If string is 100 letters long, the server will crash
-            req.uri_path = random.choice(["/basic/", "/storage/", "/separate/", "/long/", "/big/", "/void/", "/xml/", "/encoding/", "/etag/", "/child/", "/advanced/", "/advancedSeparate/"])
-            req.payload = fuzzed_payload
-            req.destination = (self.host, self.port)
-            req._mid = random.randint(1, 65535) #required, don't change
-            print(req.pretty_print())
-            with open ("fuzzed requests.txt", "a") as f:
-                f.write("Request:\n" + req.pretty_print())
-                f.write("\n")
-            datagram = serializer.serialize(req) 
-            sock.sendto(datagram, req.destination)
-            datagram, source = sock.recvfrom(4096)
-            received_message = serializer.deserialize(datagram, source)# response
-            print(received_message.pretty_print())
-            with open ("fuzzed requests.txt", "a") as f:
-                f.write("Received:\n" + received_message.pretty_print())
-                f.write("\n")
-            sleep(1)
+                # Send fuzzed GET request with the fuzzed payload and path "/basic/"
+                req = Request()
+                serializer = Serializer()
+                sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                req.type = random.choice([defines.Types["CON"], defines.Types["NON"], defines.Types["ACK"], defines.Types["RST"]])
+                req.code = random.choice([defines.Codes.EMPTY.number, defines.Codes.GET.number, defines.Codes.POST.number, defines.Codes.PUT.number, defines.Codes.DELETE.number]) # Everytime EMPTY is chosen, the server will give up, but not crash
+                req.token = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(random.randint(1, 100))) # If string is 100 letters long, the server will crash
+                req.uri_path = random.choice(["/basic/", "/storage/", "/separate/", "/long/", "/big/", "/void/", "/xml/", "/encoding/", "/etag/", "/child/", "/advanced/", "/advancedSeparate/"])
+                req.payload = fuzzed_payload
+                req.destination = (self.host, self.port)
+                req._mid = random.randint(1, 65535) #required, don't change
+                print(req.pretty_print())
+                with open ("fuzzed requests.txt", "a") as f:
+                    f.write("Request:\n" + req.pretty_print())
+                    f.write("\n")
+                datagram = serializer.serialize(req) 
+                sock.sendto(datagram, req.destination)
+                datagram, source = sock.recvfrom(4096)
+                received_message = serializer.deserialize(datagram, source)# response
+                print(received_message.pretty_print())
+                with open ("fuzzed requests.txt", "a") as f:
+                    f.write("Received:\n" + received_message.pretty_print())
+                    f.write("\n")
+                sleep(1)
+            except Exception as e:
+                print("Server crashed. Restarting")
+                restart_server()
+                self.client = HelperClient(server=(self.host, self.port))
+                serializer = Serializer()
+                continue
 
     def close_connection(self):
         self.client.stop()
